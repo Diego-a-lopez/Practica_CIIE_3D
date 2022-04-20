eb68dd5a6c3892d7dbc36a35087b554eff69672e
DanaG5 <78797149+DanaG5@users.noreply.github.com>
2022-04-20 19:34:50 +0200

    Añadidos scripts
    Script que controla el HP del jugador y script que actualiza la UI acorde a ello

---
+++ MyProject/Assets/Scrpts/PlayerHealth.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public int maxHP; //the player max health, this should be assigned 5 aprox
+    public int currentHP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHP = maxHP; //player starts full health
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentHP <= 0) {
+            gameObject.SetActive(false); //this code is temporal, when the player dies is desactivated
+        }
+    }
+
+    //this function is called by the enemies when player collides with them
+    public void HurtPlayer(int damage) { //damage is the damage dealt, normally 1
+        currentHP -= damage;
+
+    }
+}
---
+++ MyProject/Assets/Scrpts/UIManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    public Slider hpbar; //assign the object HPbar of the canvas
+    public Text hptext; //assing the object HPnums
+    public PlayerHealth playerHealth; //as the script is assigned to the dog, assign here the dog
+
+    private static bool uiExists; //this will keep the hp bar and text when the player changes to another scene, otherwise it'd dissapear in the new scene
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         if (!uiExists) {
+            uiExists = true;
+            DontDestroyOnLoad(transform.gameObject);
+        } else {
+            Destroy(gameObject); //this will port or destroy the hp bar at scene change
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hpbar.maxValue = playerHealth.maxHP; //the bar's top value will be the maxHP of the player
+        hpbar.value = playerHealth.currentHP; //bar will fill until the current hp of the player
+        hptext.text = "HP: " + playerHealth.maxHP + "/" + playerHealth.currentHP; //text will show hp in format HP: x/5
+    }
+}
