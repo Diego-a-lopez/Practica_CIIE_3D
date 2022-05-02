6dfffe014c93eefeb2a3c9e3b093d66ad69ec412
DanaG5 <78797149+DanaG5@users.noreply.github.com>
2022-05-02 16:30:07 +0200

    Añadidos scripts HurtPlayer y PlayerHealth

--- MyProject/Assets/Scrpts/PlayerHealth.cs
+++ MyProject/Assets/Scrpts/PlayerHealth.cs
@@ -2,48 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class PlayerHealth : MonoBehaviour
 {
-    int cooldown;
-    public int maxHP = 100; //the player max health, this should be assigned 5 aprox
+
+    public int maxHP; //the player max health, this should be assigned 5 aprox
     public int currentHP;
-    private Director director;
-
-    GameObject Obj;
-    public Player player;
-
-
-    public void LowerHp()
-    {
-        if (cooldown < 0) { currentHP += -10; cooldown = 500;}
-        //currentHP += -10;
-        //if (currentHP < 0) Destroy(this.gameObject);
-    }
-
-    public void IncreaseHp()
-    {
-        currentHP += 10;
-    }
-
-
 
     // Start is called before the first frame update
     void Start()
     {
-        cooldown = 1000;
-        maxHP = 100;
         currentHP = maxHP; //player starts full health
-        Obj = GameObject.Find("Director");
-        director = Obj.GetComponent<Director>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (currentHP <= 0) {
-            //gameObject.SetActive(false); //this code is temporal, when the player dies is desactivated
-            director.GoToMain(); 
+            gameObject.SetActive(false); //this code is temporal, when the player dies is desactivated
         }
     }
 
@@ -52,10 +27,4 @@ public class PlayerHealth : MonoBehaviour
         currentHP -= damage;
 
     }
-
-    void FixedUpdate() {
-        cooldown = cooldown  - 100;
-
-    }
-
 }
