9c66e731fb06d6286f861d4c4d57c31739d44f6e
j-p-martinez935 <72380801+j-p-martinez935@users.noreply.github.com>
2022-05-08 16:49:40 +0200

    [Player.cs] IMPORTANTE
    Player escencialmente acabado. Las colisiones ahora se detectan con todo aquello que esté en el LAYER ground. Por lo tanto añadir un layer Ground y meter en este todos los objetos que se quieran poder PISAR es necesario. Si quieres saltar en un objeto es posible que el otro (no el player) sólo tenga un collider. Sin embargo si estamos encima de algo que no esté en el layer GROUND no será posible saltar. Cuidado con ese detalle.

--- MyProject/Assets/Scrpts/Player.cs
+++ MyProject/Assets/Scrpts/Player.cs
@@ -4,113 +4,104 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    //Controllers for the player movement speed, jump, fall, etc.
+    //Controllers for the player movement speed, turning, jumping, falling, and
+    //manual gravity since we are not working with rigidbodies.
     public float speed = 5f;
-    public float rotationSpeed = 720f;
-    public float jumpVelocity = 6f;
+    public float turnSmoothT = 0.1f;
+    public float turnSmoothV;
+    public float jumpVelocity = 2f;
     public float fallMultiplier = 2.5f;
     public float lowJumpMultiplier = 2f;
+    public float gravity = -9.81f;
+    Vector3 velocity;
 
-    //Booleans for jump controll.
-    private bool jumpKeyWasPressed;
-    private bool grounded = true;
+    
+
+    //Variables for ground check and jumping.
+    public bool grounded;
+    public Transform groundCheck;
+    public float groundDistance = 0.1f;
+    public LayerMask groundMask;
+
     //Extra stuff that does not fit previous categories.
+    public CharacterController controller;
+    public Transform cam;
     public GameObject ODirector;
-    private GameAssets assets;
-    private Rigidbody rigidBody;
-    private Director director;
+    public GameAssets assets;
+    public Director director;
 
     // Start is called before the first frame update
     void Start()
     {
         GameObject Obj;
-        rigidBody = GetComponent<Rigidbody>();
         //ODirector = GameObject.Find("Director");
         Obj = GameObject.Find("GameAssets");
         assets = Obj.GetComponent<GameAssets>();
-        director = ODirector.GetComponent<Director>();
+        director = Obj.GetComponent<Director>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Check to assign value to grounded variable and determine where it is.
+        grounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if(grounded && velocity.y < 0){
+            //If it's on the ground, keep it on there.
+            velocity.y = -2f;
+        }
         //Getting Horizontal and Vertical axis for player movement.
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
 
-        //Vector direction that controlls player movement in the x and y axis (or x and z)
-        Vector3 movementDirection = new Vector3(horizontalInput,0,verticalInput);
-        movementDirection.Normalize();
+        //Vector direction that controlls player movement in the x and z axis.
+        Vector3 movementDirection = new Vector3(horizontalInput,0,verticalInput).normalized;
 
-        //Movement controller with the translation. Uses the speed and direction as well
-        //as world coordinates to know which direction is which.
-        transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
+        //Calculating general jump ecuation. Before adding gravity so it doesn't
+        //cancel it out.
+        if(Input.GetButtonDown("Jump") && grounded){
+            velocity.y = Mathf.Sqrt(jumpVelocity * -2f * gravity);
+            assets.PlayJump();
+        }
 
-        //If we are moving towards something, we rotate the character, if neccessary.
-        //The quaternion rotation makes it so we look at the place we clicked, and since
-        //its aditive, we can make sure we always look at the correct way when rotating.
-        if(movementDirection != Vector3.zero){
+        //Adding the gravity permanently before any other movement other than jump.
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
 
+        //Rotation is cursor dependant. The forward movement will always be where the
+        //camera is looking at.
+        if(movementDirection.magnitude >= 0.1f){
 
-            Quaternion toRotation = Quaternion.LookRotation(movementDirection,Vector3.up);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed*Time.deltaTime);
+            float targetAngle = Mathf.Atan2(movementDirection.x, movementDirection.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            //Making the turning of the camera more smooth so its not extremely sudden.
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothV, turnSmoothT);
+            transform.rotation = Quaternion.Euler(0f, angle,0f);
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            //We make the character turn and the camera if neccesary
+            controller.Move(moveDir * speed * Time.deltaTime);
 
         }
 
-        //Control for jump and check if we are in the ground as an extra.
-        if(Input.GetButtonDown("Jump") && grounded){
-            assets.PlayJump();
-           jumpKeyWasPressed = true;
+        //Fast version of the fall after jumping. Also works for falling of a ledge.
+        if(velocity.y < 0){
+            velocity.y += gravity * (fallMultiplier -1) * Time.deltaTime; 
+        }
+        //Slow version of the fall. Condition of getButton Jump as to check the 
+        //user is not holding key, and apply different physics of falling.
+        else if(velocity.y > 0 && !Input.GetButton("Jump")){
+            velocity.y += gravity * (lowJumpMultiplier - 1) * Time.deltaTime; 
         }
 
         //Director call so we can call the menu.
         if (Input.GetKeyDown(KeyCode.Escape)){
-           director.GoToMain();
+            director.GoToMain();
         }
     }
-
-
-    private void FixedUpdate() {
-
-        //Condition check in case we just drop down without jumping. The multiplier for
-        //falling would activate regardless.
-        if(rigidBody.velocity.y < 0){
-            rigidBody.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
-        } 
-        //If we are not falling we can check if we are jumping, so it would use a 
-        //different multiplier and make the fall less sudden.
-        else if (rigidBody.velocity.y > 0 && !Input.GetButton("Jump")){
-            rigidBody.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;            
-        }
-
-        //Actions that are taken when the jumpkey is pressed, 
-        //using addForce so its a sudden burst of upwards momentum. 
-        //We also change the values of the booleans so the player doesn't jump infinitely.
-        if(jumpKeyWasPressed){
-             rigidBody.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
-             jumpKeyWasPressed = false;
-             grounded = false;
-        }
-    }
-
-    //Checks collision with ground to enable jump. The Object MUST be tagged.
-    private void OnCollisionStay(Collision collision){
-        if(collision.gameObject.tag == "Ground"){
-            grounded = true;
-        }
-    }
-
-    //Checks exit of the collision state so jumping again is enabled.
-    private void OnCollisionExit(Collision collision){
-        if(grounded){
-            grounded = false;
-        }
-    }
-    
 }
 
           
 
+    
     
     
 
